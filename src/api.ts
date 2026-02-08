2860a57de5084cdae4a7d46853a9bed4c91c9cf0
885wkh9dny-stack <janoschell@gmx.de>
2026-02-08 19:37:47 +0100

    Add files via upload

---
+++ src/api.ts
@@ -0,0 +1,9 @@
+const API = "http://localhost:8000";
+export async function post(path: string, data: any) {
+  const res = await fetch(API + path, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  return res.json();
+}
