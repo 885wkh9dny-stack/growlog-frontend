fc7dd8fe2610cb1290c3dadc83090fa5ea570bce
885wkh9dny-stack <janoschell@gmx.de>
2026-02-08 19:35:02 +0100

    Delete src directory

--- src/api.ts
+++
@@ -1,9 +0,0 @@
-const API = "http://localhost:8000";
-export async function post(path: string, data: any) {
-  const res = await fetch(API + path, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  });
-  return res.json();
-}
